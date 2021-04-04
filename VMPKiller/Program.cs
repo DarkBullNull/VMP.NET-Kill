8c053981a11ff6b91860e37418a242d10ac0fc85
Maxim <43318993+DarkBullNull@users.noreply.github.com>
2021-04-04 22:03:09 +0300

    Nothing
    Nothing

--- VMPKiller/Program.cs
+++ VMPKiller/Program.cs
@@ -21,26 +21,6 @@ namespace VMPKiller
 {
     class Program
     {
-        static int GetPositionAfterMatch(byte[] data, byte[]pattern)
-        {
-            for (int i = 0; i < data.Length - pattern.Length; i++)
-            {
-                bool match = true;
-                for (int k = 0; k < pattern.Length; k++)
-                {
-                    if (data[i + k] != pattern[k])
-                    {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match)
-                {
-                    return i;
-                }
-            }
-            return 0;
-        }
         static void Main(string[] args)
         {
             Console.Title = "########################";
